c7740eda073a7f66f77361e46c185d0a1019abc1
ilja.tkachuk <tkachuk.labs@gmail.com>
2020-10-08 15:28:26 +0300

    heathmont org

--- release.sh
+++ release.sh
@@ -3,6 +3,6 @@
 set -e
 
 for V in "2.3.7"; do
-  docker build -t "coingaming/nix:$V" -f "./Dockerfile-nix:$V" .
-  docker push "coingaming/nix:$V"
+  docker build -t "heathmont/nix:$V" -f "./Dockerfile-nix:$V" .
+  docker push "heathmont/nix:$V"
 done
